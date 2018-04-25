567b0f763983fe2571f476a8a0a03975eb6cfe47
Alec McLeod <aimcleod@ucsc.edu>
2018-04-24 22:10:41 -0700

    Added proof of concept API function to home page

--- sccresources/sccalendar/google_auth.py
+++ sccresources/sccalendar/google_auth.py
@@ -3,7 +3,7 @@ from google.oauth2 import service_account
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-import datetime
+
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 SERVICE_ACCOUNT_FILE = 'sccalendar/servicekey.json'
@@ -12,17 +12,8 @@ credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 service = build('calendar', 'v3', credentials=credentials)
-#service.setServiceAccountUser(sccresources@starry-strand-202203.iam.gserviceaccount.com)
 
-now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-print('Getting the upcoming 10 events')
-events_result = service.events().list(calendarId='ucsc.edu_gn5gb46mq2mt6961h0g3jifakg@group.calendar.google.com', timeMin=now,
-                                      maxResults=10, singleEvents=True,
-                                      orderBy='startTime').execute()
-events = events_result.get('items', [])
+def getService():
+    return service
 
-if not events:
-    print('No upcoming events found.')
-for event in events:
-    start = event['start'].get('dateTime', event['start'].get('date'))
-    print(start, event['summary'])
+
--- sccresources/sccalendar/views.py
+++ sccresources/sccalendar/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from . import google_auth
+import datetime
 
 # Create your views here.
+
+
 def index(request):
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    # Retrieve next ten events starting from now from the linked google calendar
+    events_result = google_auth.getService().events().list(calendarId='ucsc.edu_gn5gb46mq2mt6961h0g3jifakg@group.calendar.google.com', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    # Parse the events into a simple printable form
+    eventsSimple = []
+    for event in events:
+        eventsSimple.append(event['start'].get('dateTime', event['start'].get('date')))
     return render(
         request,
         'index.html',
         #passes the contents of the brackets to the template
-        context={},
+        context={'eventsSimple':eventsSimple},
     )
