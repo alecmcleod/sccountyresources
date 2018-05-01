97e9afedfa88002909beaf1a03e6f0a2ee656e55
mobile-bungalow <Paul@dhcp-169-233-181-236.cruznet.ucsc.edu>
2018-05-01 16:11:24 -0700

    functional details page

--- sccresources/sccalendar/views.py
+++ sccresources/sccalendar/views.py
@@ -7,19 +7,78 @@ import datetime
 
 def index(request):
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
     # Retrieve next ten events starting from now from the linked google calendar
-    events_result = google_auth.get_service().events().list(calendarId='ucsc.edu_gn5gb46mq2mt6961h0g3jifakg@group.calendar.google.com', timeMin=now,
+    events_result = google_auth.get_service().events().list(calendarId='hv4cl31tra0t7l0ggbfrev6tes@group.calendar.google.com', timeMin=now,
                                                             maxResults=10, singleEvents=True,
                                                             orderBy='startTime').execute()
     events = events_result.get('items', [])
     # Parse the events into a simple printable form
     eventsSimple = []
+
     for event in events:
-        eventsSimple.append(event['start'].get('dateTime', event['start'].get('date')))
+        eventsSimple.append((event['start'].get('dateTime'), event['start'].get('date'), event.get('id')))
     return render(
         request,
         'index.html',
         #passes the contents of the brackets to the template
         context={'eventsSimple':eventsSimple},
     )
+
+def details(request , event_id=None):
+    
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    # Retrieve next ten events starting from now from the linked google calendar
+    events_result = google_auth.get_service().events().list(calendarId='hv4cl31tra0t7l0ggbfrev6tes@group.calendar.google.com', timeMin=now,
+                                                            maxResults=10, singleEvents=True,
+                                                            orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    # Parse the events into a simple printable form
+
+    title = None
+
+    location = None
+
+    text = None
+
+    found = False
+
+    if event_id == None:
+        return render(request,'404.html')
+
+    for event in events:
+        print(event.get('id'))
+        if event.get('id') == event_id:
+            found = True
+            title = event.get('summary')
+            location = event.get('location')
+            text = event.get('description')
+            date = event['start'].get('dateTime')
+            time = event['start'].get('date')
+            if text == None:
+                text = 'no description'
+            if title == None:
+                title = 'no title'
+            if location == None:
+                location ='1515 Ocean St, Santa Cruz, CA 95060'
+            break
+
+
+    tags = ['tag1','tag2','tag3','tag4']
+
+    ##in future add a method to auto generate tags from location
+    ## wait for alec to add etags in events
+
+    if not found:
+        return render(request,'404.html')
+
+
+    return render(request,'details.html',context={'title':title,
+'location':location,'description':text,'date':date,'time':time,'tags':tags})
+
+
+
+
+
+
+
+
