473971f7f9a1c22d06f3a8a331216ae6d610e443
Alec McLeod <aimcleod@ucsc.edu>
2018-05-02 00:30:48 -0700

    added search functionality for desired service

--- sccresources/sccalendar/forms.py
+++ sccresources/sccalendar/forms.py
@@ -1,4 +1,13 @@
 from django import forms
 
+
 class SearchForm(forms.Form):
-    your_name = forms.CharField(label='Test field', max_length=100)
+
+    # Elements inside SERVICES correspond to (option_value, option_text)
+    SERVICES_CHOICES = (('', ''), ('DRUGS', 'Drug Support'), ('FOOD', 'Food'), ('HEALTH', 'Health Care'), ('SHOWER', 'Showers'))
+    # TODO replace option_value for LOCATION_CHOICES with google api location strings
+    LOCATION_CHOICES = (('', ''), ('SANTACRUZ', 'Santa Cruz'), ('WATSONVILLE', 'Watsonville'), ('SCOTTSVALLEY', 'Scotts Valley'),
+                        ('FELTON', 'Felton'), ('BOULDERCREEK', 'Boulder Creek'), ('DAVENPORT', 'Davenport'))
+
+    services = forms.ChoiceField(label='I need', widget=forms.Select, choices=SERVICES_CHOICES, required=False)
+    locations = forms.ChoiceField(label='near', widget=forms.Select, choices=LOCATION_CHOICES, required=False)
--- sccresources/sccalendar/urls.py
+++ sccresources/sccalendar/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('details/<str:event_id>', views.details, name='details'),
+    path('details/<str:service>/<str:event_id>', views.details, name='details'),
+    path('search/', views.search, name='search'),
 ]
