42a2b4eb1881ee1084bf97205878bb0e8b339825
Alec McLeod <aimcleod@ucsc.edu>
2018-05-02 17:12:50 -0700

    added google maps api key

--- sccresources/sccalendar/forms.py
+++ sccresources/sccalendar/forms.py
@@ -6,8 +6,8 @@ class SearchForm(forms.Form):
     # Elements inside SERVICES correspond to (option_value, option_text)
     SERVICES_CHOICES = (('', ''), ('DRUGS', 'Drug Support'), ('FOOD', 'Food'), ('HEALTH', 'Health Care'), ('SHOWER', 'Showers'))
     # TODO replace option_value for LOCATION_CHOICES with google api location strings
-    LOCATION_CHOICES = (('', ''), ('SANTACRUZ', 'Santa Cruz'), ('WATSONVILLE', 'Watsonville'), ('SCOTTSVALLEY', 'Scotts Valley'),
-                        ('FELTON', 'Felton'), ('BOULDERCREEK', 'Boulder Creek'), ('DAVENPORT', 'Davenport'))
+    LOCATION_CHOICES = (('', ''), ('Santa Cruz, CA', 'Santa Cruz'), ('Watsonville, CA', 'Watsonville'), ('Scotts Valley, CA', 'Scotts Valley'),
+                        ('Felton, CA', 'Felton'), ('Boulder Creek, CA', 'Boulder Creek'), ('Davenport, CA', 'Davenport'))
 
     services = forms.ChoiceField(label='I need', widget=forms.Select, choices=SERVICES_CHOICES, required=False)
     locations = forms.ChoiceField(label='near', widget=forms.Select, choices=LOCATION_CHOICES, required=False)
--- sccresources/sccalendar/google_auth.py
+++ sccresources/sccalendar/google_auth.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from google.oauth2 import service_account
 from apiclient.discovery import build
 
+# maps api key AIzaSyDY3_muYN8O6uGzGGRE35Xj_OPAMVrup4g
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 SERVICE_ACCOUNT_FILE = 'sccalendar/servicekey.json'
