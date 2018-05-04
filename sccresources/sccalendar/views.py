8dfcd5f9106d30cd6cd1138016286b0c54098b4c
Unknown <pwmay@ucsc.edu>
2018-05-04 14:18:18 -0700

    Auto stash before merge of "master" and "origin/master"
    added requested changes to details page, fixed base generic.

--- sccresources/sccalendar/views.py
+++ sccresources/sccalendar/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from . import google_auth
 from datetime import datetime, time, timedelta
 from .forms import SearchForm
+import calendar
 
 
 # Calendar ID variables
@@ -76,7 +77,64 @@ def search(request):
         )
 
 
+
 def details(request, service=None, event_id=None):
+    '''Details: returns a response with all event info'''
+    '''         pretaining to an event with id 'event_id' '''
+
+    def parse_recurrance(rec_list):
+        '''arguments: standard google calendars list of recurrance strings'''
+        '''output : an english string describing when an event recurrs'''
+        keys = ['FREQ','COUNT','INTERVAL','BYDAY','UNTIL']
+        parse = ''
+        out_string = ''
+        for rule in rec_list:
+            for key in keys:
+                if key in rule:
+                    parse = rule.split('=')
+                    if parse[0] is 'FREQ':
+                        parse = parse[1].lower() + ', '
+                    elif parse[0]  is 'COUNT':
+                        parse = ''
+                    elif parse[0]  is 'INTERVAL':
+                        parse = ''
+                    elif parse[0]  is 'BYDAY':
+                        parse = 'on' + to_sent(parse[1])
+                    elif parse[0]  is 'UNTIL':
+                        parse = 'until' + calendar.month_name(int(parse[1][5:6])) + ',' + parse[1][7:]
+                    else:
+                        break
+            out_string += ("this event occurs " + parse)
+        return out_string
+
+    def to_sent(abbrv_string):
+        '''to_sent: input: a comma seperate string of day abbreviations
+                    output: an english sentence enumerating those days'''
+        abbrv_dict = {'MO':'Monday','TU':'Tuesday','WE':'Wednesday'
+        ,'TH':'Thursday','FR':'Friday','SA':'Saturday','SU':'Sunday'}
+        
+        abbrv_list = abbrv_string.split(',')
+        
+        length = len(abbrv_list)-1
+        
+        sent = ' '
+
+        for i  in range(length):
+           sent += abbrv_dict[abbrv_list[i]]
+
+        sent += ("and " + abbrv_dict[length+1])
+
+        return sent
+
+
+    def to_standard(military_string):
+        '''to_standard: converts military time to standard and adds meridean'''
+
+        military_list = military_string.split(':')
+        if int(military_list[0]) > 12:
+            return str( int(military_list[0])-12 ) + ':' + military_list[1] + " P.M."
+        else:
+            return str( int(military_list[0]) ) + ':' + military_list[1] + " A.M."
 
     if service == 'DRUGS':
         event = google_auth.get_service().events().get(calendarId=DRUG_CAL, eventId=event_id).execute()
@@ -93,28 +151,62 @@ def details(request, service=None, event_id=None):
         return render(request, '404.html')
 
     title = event.get('summary')
+    try:
+        recurrence = event[recurrence]
+        recurrence = parse_recurrance(recurrence)
+        print(recurrence)
+    except:
+        recurrence = None
+
     location = event.get('location')
     text = event.get('description')
     event_date = event['start'].get('dateTime')
     event_time = event['start'].get('date')
+
+    #if block which replaces the default values with empty strings
+
     if text is None:
-        text = 'no description'
+        text = ''
+
     if title is None:
-        title = 'no title'
+        title = ''
+
     if location is None:
         location = '1515 Ocean St, Santa Cruz, CA 95060'
 
-    tags = ['tag1', 'tag2', 'tag3', 'tag4']
+    if text is None:
+        text = ''
 
-    # in future add a method to auto generate tags from location
-    # wait for alec to add etags in events
+    if recurrence is None:
+        recurrence = ''
+
+    if event_time is None:
+        event_time = ''
+    elif '-' in event_time:
+        ed_list = event_time.split('-')
+        ed_list[2] = ed_list[2].split('T')[0] 
+        event_time = calendar.month_name[int(ed_list[1])] + ' , ' + ed_list[2] + ' , ' + ed_list[0]
+    elif ':' in event_time:
+        event_time = to_standard(event_time) 
+
+    # ^ ^ this requires explanation. some people think its fun to enter
+    #the time as the date or the date as the time. this is to fix monkey
+    #problems.
+
+    if event_date is None:
+        event_date = ''
+    else:
+        ed_list = event_date.split('-')
+        ed_list[2] = ed_list[2].split('T')[0] 
+        event_date = calendar.month_name[int(ed_list[1])] + ' , ' + ed_list[2] + ' , ' + ed_list[0]
 
     return render(request, 'details.html', context={'title': title,
                                                     'location': location,
                                                     'description': text,
                                                     'date': event_date,
                                                     'time': event_time,
-                                                    'tags': tags})
+                                                    'recurrence':recurrence
+                                                    })
 
 
 
