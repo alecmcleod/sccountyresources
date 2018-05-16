08c1a3894eb6367b697a9c5dd7081e858b8febcc
Isaac Avram <avrisaac555@gmail.com>
2018-05-16 14:30:31 -0700

    Added code to test exporting the file

--- sccresources/sccalendar/tests.py
+++ sccresources/sccalendar/tests.py
@@ -1,6 +1,8 @@
+import tempfile
+import os
+from sys import platform
 from .calendar import GoogleCalendar
 from .google_auth import get_service
-
 from django.test import TestCase
 # Create your tests here.
 class GoogleCalendarTestCase(TestCase):
@@ -12,4 +14,20 @@ class GoogleCalendarTestCase(TestCase):
         c = GoogleCalendar(self.service, self.calendar_id)
         ical = c.export_ical()
 
-        self.assertEquals(len(ical.subcomponents), 5)
+        # Sanity Check
+        self.assertEqual(len(ical.subcomponents), 5)
+        
+        # Export the calendar as a file
+        directory = tempfile.mkdtemp()
+        path = os.path.join(directory, 'artifact.ics')
+        f = open(path, 'wb')
+        f.write(ical.to_ical())
+        f.close()
+        print("Created test artifact " + path)
+
+        if platform == "darwin":
+            # Open the ical file on OSX
+            os.system("open " + path)
+        elif platform == "win32":
+            # Open the ical file on Windows
+            os.system("start " + path)
