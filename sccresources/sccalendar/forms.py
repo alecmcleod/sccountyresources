81ff1ed7a67d51fde4efb7571d82cccf46020961
Alec McLeod <aimcleod@ucsc.edu>
2018-06-05 22:42:31 -0700

    Removed sjango form error messages and made a services selection required for search

--- sccresources/sccalendar/forms.py
+++ sccresources/sccalendar/forms.py
@@ -10,14 +10,16 @@ class SearchForm(forms.Form):
     LOCATION_CHOICES = (('', ''), ('Santa Cruz, CA', 'Santa Cruz'), ('Watsonville, CA', 'Watsonville'), ('Scotts Valley, CA', 'Scotts Valley'),
                         ('Felton, CA', 'Felton'), ('Boulder Creek, CA', 'Boulder Creek'), ('Davenport, CA', 'Davenport'))
 
-    services = forms.ChoiceField(label='I need', choices=SERVICES_CHOICES, required=False, label_suffix="", widget=forms.Select(attrs={'id': 'services'}))
+    services = forms.ChoiceField(label='I need', choices=SERVICES_CHOICES, required=True, label_suffix="", widget=forms.Select(attrs={'id': 'services'}), error_messages={'required': ''})
     locations = forms.ChoiceField(label='near', choices=LOCATION_CHOICES, required=False, label_suffix="", widget=forms.Select(attrs={'id':'locations'}))
+
 
 class SubscribeForm(forms.Form):
 
-	phone_number = PhoneNumberField(max_length=15, min_length=10,\
-								label='Enter Phone Number' , required=True)
+    phone_number = PhoneNumberField(max_length=15, min_length=10,\
+                                    label='Enter Phone Number' , required=True)
+
 
 class ConfirmForm(forms.Form):
 
-	code = forms.CharField(widget=forms.NumberInput, max_length=4, min_length=4, required=True)
+    code = forms.CharField(widget=forms.NumberInput, max_length=4, min_length=4, required=True)
