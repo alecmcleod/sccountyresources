e4c894640f64d373becb2f2383c4043c7a7c0d02
Isaac Avram <avrisaac555@gmail.com>
2018-06-06 17:47:30 -0700

    Fixed incorrect url config

--- sccresources/sccalendar/urls.py
+++ sccresources/sccalendar/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('details/<str:service>/<str:event_id>', views.details, name='details'),
+    path('details/<str:service>/<str:event_id>/', views.details, name='details'),
     path('search/', views.search, name='search'),
-    path('download/event/<str:service>/<str:event_id>', views.event_ical_download, name='download_event'),
-    path('download/calendar/<str:service>', views.calendar_ical_download, name='download_calendar'),
+    path('download/event/<str:service>/<str:event_id>/', views.event_ical_download, name='download_event'),
+    path('download/calendar/<str:service>/', views.calendar_ical_download, name='download_calendar'),
     path('calendars/', views.calendars, name='calendars'),
     path('confirm/', views.subscribe, name='confirm'),
     path('confirm/result/', views.confirm, name='result'),
