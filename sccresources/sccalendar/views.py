03924c7aafd1b7079a6d424e70d190a47d602a59
Isaac Avram <avrisaac555@gmail.com>
2018-06-06 18:41:47 -0700

    Removed unecessary catch statement

--- sccresources/sccalendar/views.py
+++ sccresources/sccalendar/views.py
@@ -90,13 +90,10 @@ def search(request):
         # If no location is provided, don't apply distance to the event
         events_today = list(var_map[services].get_events(api_params))
     else:
-        try:
-            # Use Calendar API to get a list of GoogleEvents, then use Distance Matrix to add distances to those events
-            events_today = gmaps.convert_events(locations, list(var_map[services].get_events(api_params)))
-            if events_today is not None:
-                sort_events(events_today)
-        except ValueError:
-            return HttpResponseBadRequest("<h1>400 Bad Request</h1><p>Invalid location.</p>")
+        # Use Calendar API to get a list of GoogleEvents, then use Distance Matrix to add distances to those events
+        events_today = gmaps.convert_events(locations, list(var_map[services].get_events(api_params)))
+        if events_today is not None:
+            sort_events(events_today)
 
     return render(
         request,
