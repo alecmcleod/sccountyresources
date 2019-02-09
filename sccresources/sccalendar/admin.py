84cc600c1a4b9158f52843c0428ace16a1ea66e7
alecmcleod <aimcleod@ucsc.edu>
2019-02-09 11:11:45 -0800

    Created model for static event and registered it with admin

--- sccresources/sccalendar/admin.py
+++ sccresources/sccalendar/admin.py
@@ -1,3 +1,5 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from sccalendar.models import StaticEvent
+admin.site.register(StaticEvent)
--- sccresources/sccalendar/models.py
+++ sccresources/sccalendar/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Event(models.Model):
@@ -10,3 +11,23 @@ class Event(models.Model):
 class Number(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     number = models.CharField(max_length=13)
+
+class StaticEvent(models.Model):
+    """ Defines non-changing events that can be displayed as a list"""
+
+    # Fields
+    event_name = models.CharField(max_length=100, help_text='Enter event name')
+    event_details = models.CharField(max_length=200, help_text='Enter event details (times ect.)', blank=True, null=True)
+    event_address = models.CharField(max_length=200, blank=True, null=True, help_text='Enter event address')
+    event_phone = PhoneNumberField(blank=True, null=True)
+    event_email = models.EmailField(blank=True, null=True)
+    event_url = models.URLField(max_length=200, blank=True, null=True)
+
+    # Metadata
+    class meta:
+        ordering = ['event_name']
+
+    # Methods
+    def __str__(self):
+        return self.event_name
+
