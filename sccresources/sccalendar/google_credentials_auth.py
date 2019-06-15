8727d9c65e734835a9185d74edb5d5d0ce52c2d8
jesse <brennan@pacific.net>
2019-06-15 15:21:46 -0700

    Use separate API key for server and client when deploying

--- sccresources/sccalendar/google_credentials_auth.py
+++ sccresources/sccalendar/google_credentials_auth.py
@@ -17,12 +17,23 @@ def get_google_service_credentials():
         raise ValueError('Set GOOGLE_SERVICE_KEY to allow Google API access')
 
 
-def get_google_api_key():
-    """Return api key or raise ValueError"""
+def get_google_api_key(dest):
+    """
+    Return api key or raise ValueError
+
+    dest may be 'server' or 'client'
+    """
+    if dest not in ('server', 'client'):
+        raise ValueError("dest must be 'server' or 'client'")
     try:
-        return os.environ['GOOGLE_MAPS_KEY']
+        # For local deployment, only one key needs to be used
+        return os.environ[f'GOOGLE_MAPS_KEY']
     except KeyError:
-        raise ValueError('Set GOOGLE_MAPS_KEY to allow Google API access')
+        pass
+    try:
+        return os.environ[f'GOOGLE_MAPS_KEY_{dest.upper()}']
+    except KeyError:
+        raise ValueError(f'Set GOOGLE_MAPS_KEY[_{dest.upper()}] to allow Google API access')
 
 
 credentials = service_account.Credentials.from_service_account_file(
